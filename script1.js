1bc012ac50abaa7a73b7864b1e135efebf25f34a
armaan4658 <80997749+armaan4658@users.noreply.github.com>
2021-05-17 11:25:41 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -6,16 +6,10 @@ for(let i = 0; i < 5; i++){
   row.setAttribute("class","row");
   var col1 = document.createElement("div");
   col1.setAttribute("class","col-sm-4 col-md-4 col-lg-4 col-xl-4");
-  var img1 = document.createElement("img");
-  //col1.append(img1);
   var col2 = document.createElement("div");
   col2.setAttribute("class","col-sm-4 col-md-4 col-lg-4 col-xl-4");
-  var img2= document.createElement("img");
-  //col2.append(img2);
   var col3 = document.createElement("div");
   col3.setAttribute("class","col-sm-4 col-md-4 col-lg-4 col-xl-4");
-  var img3 = document.createElement("img");
-  //col3.append(img3);
   row.append(col1,col2,col3);
   container.append(row);
 }
@@ -23,7 +17,6 @@ document.body.append(container);
 //adding name and images in html
 function addContent(){
     var col = document.querySelectorAll(".row > div");
-    var img = document.querySelectorAll("img");
     fetch('https://609e2a6533eed80017957e03.mockapi.io/user',{method:'GET'})
     .then(data=>data.json())
     .then(data=>{
